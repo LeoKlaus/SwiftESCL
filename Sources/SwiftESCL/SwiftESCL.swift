2dfe1eb005eba1f0a5312c9f18662b7ea55f5cea
Leo Wehrfritz <leocommander.cs@gmail.com>
2022-08-09 14:30:22 +0200

    initial commit

--- Sources/SwiftESCL/SwiftESCL.swift
+++
@@ -1,6 +0,0 @@
-public struct SwiftESCL {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
--- Tests/SwiftESCLTests/SwiftESCLTests.swift
+++ Tests/SwiftESCLTests/SwiftESCLTests.swift
@@ -6,6 +6,6 @@ final class SwiftESCLTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(SwiftESCL().text, "Hello, World!")
+        //XCTAssertEqual(SwiftESCL().text, "Hello, World!")
     }
 }
